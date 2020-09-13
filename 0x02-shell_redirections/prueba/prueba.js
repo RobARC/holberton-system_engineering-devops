c6bcd8278f7afb88d25ffb539016d7570e0e8fc6
RobARC <rrondonc@gmail.com>
2020-09-13 16:34:58 -0500

    Project Direcory

---
+++ 0x02-shell_redirections/prueba/prueba.js
@@ -0,0 +1,7 @@
+#This is de README file to the project 0x02. Shell, I/O Redirections and filters
+
+#Tasks0. Hello World script that prints “Hello, World”, followed by a new line to the standard
+# output.
+#1. Confused smiley a script that displays a confused smiley "(Ôo)'
+#2. Let's display a file a escript that display the content of the /etc/passwd file.
+#3. 
