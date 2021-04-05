308466e23c4a686ef6dea4fb5c11bf2c9a43df1b
RobARC <rrondonc@gmail.com>
2021-04-05 02:07:51 +0000

    updating TAsk 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-$content = 'I love Puppet'
 file {'holberton':
   path    => '/tmp/holberton',
   owner   => www-data,
   group   => www-data,
   mode    => '0744',
-  content => $content,
+  content => 'I love Puppet',
 }
