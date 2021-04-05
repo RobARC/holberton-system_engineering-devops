f6e5e3c13c9daa5be86b55b7bb45d5e515c90421
RobARC <rrondonc@gmail.com>
2021-04-05 01:29:53 +0000

    update task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,19 @@
 #Using Puppet to make changes to our ssh configuration file
-include stdlib
 
-file { 'Turn off password auth':
+file { 'Ensure file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  mode   => '0644',
+  owner  => 'root',
+  group  => 'root'
+}
+file_line { 'Turn off password auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no'
 }
-
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentifyFile ~/.ssh/holberton'
 }
-
