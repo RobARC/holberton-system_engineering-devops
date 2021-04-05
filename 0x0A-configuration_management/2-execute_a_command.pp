0c1462d1f116d954d180482ef6a2048da3421e4a
RobARC <rrondonc@gmail.com>
2021-04-05 02:51:17 +0000

    updating task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,8 @@
 #create a manifest that kills a process named killmenow.
-exec { "Kill":
- command => 'pkill -f killmenow',
+exec { 'Kill':
+ command  => 'pkill -f killmenow',
+ path     => 'usr/bin/',
+ user     => 'root',
+ provider => 'shell',
+
 }  
