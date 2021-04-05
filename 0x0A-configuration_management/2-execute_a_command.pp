a99eb572ee9dc5ab0765fbd52719e0ee22d1b615
RobARC <rrondonc@gmail.com>
2021-04-05 17:06:17 +0000

    update task 2 changing path position

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 #create a manifest that kills a process named killmenow.
 exec { 'killmenow':
- command  => 'pkill -f killmenow',
  path     => 'usr/bin/',
+ command  => 'pkill -f killmenow',
  user     => 'root',
  provider => 'shell',
 
