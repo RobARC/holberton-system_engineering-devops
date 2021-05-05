cb497129107a0df95dc2d600bfd4f1751f5b1723
Roberth <rrondoc@gmail.com>
2021-05-05 15:15:09 -0500

    updating task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -19,7 +19,7 @@ def main(argv):
 
     with open('{}.csv'.format(user_id), 'w+') as file:
         for todo in request_todo:
-            info = '"{}", "{}", "{}", "{}"\n'.format(
+            info = '"{}","{}","{}","{}"\n'.format(
                 user_id, name, todo.get('completed'), todo.get('title'))
             file.write(info)
 
