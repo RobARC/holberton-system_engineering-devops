517a712741a25848d8e3b9f2f7b756b4ca87ddeb
Roberth <rrondoc@gmail.com>
2021-05-05 15:48:12 -0500

    uploading task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Script to export data in JSON """
+
+
+import json
+import requests
+from sys import argv
+
+
+def main(argv):
+    """ module export data in JSON """
+
+    url = 'https://jsonplaceholder.typicode.com'
+    user_id = argv[1]
+    user = url + '/users/{}'.format(user_id)
+    todos = url + '/todos/?userId={}'.format(user_id)
+    name = requests.get(user).json().get('username')
+    request_todo = requests.get(todos).json()
+    tasks = []
+
+    with open('{}.json'.format(user_id), 'w+') as file:
+            for todo in request_todo:
+                task = {"task": todo.get('title'),
+                        "completed": todo.get('completed'), "username": name}
+                tasks.append(task)
+            info = {user_id: task}
+            file.write(json.dumps(info))
+
+
+if __name__ == "__main__":
+    main(argv)
